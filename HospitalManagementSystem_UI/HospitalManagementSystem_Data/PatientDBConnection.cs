6856c8033f3070233ad90c041866410c15fa492b
Padavati <lavanyavaralu07399@gmail.com>
2022-01-20 22:34:34 +0530

    File Added

---
+++ HospitalManagementSystem_UI/HospitalManagementSystem_Data/PatientDBConnection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using HospitalManagementSystem_Entity;
+using System.Data.SqlClient;
+
+namespace HospitalManagementSystem_Data
+{
+    public class PatientDBConnection
+    {
+        string sqlConnectionStr = "Data Source=LAPTOP-DL0P9UEA;Initial Catalog=HMS;Integrated Security=True";
+        public string InsertPatientInfo(PatientInfo patientInfoObj)
+        {
+            DataTable dataTableObj = new DataTable();
+            SqlConnection sqlConnectionObj = new SqlConnection(sqlConnectionStr);
+            SqlDataAdapter sqlDataAdapterObj = new SqlDataAdapter("Insert into PatientInfo values('" + patientInfoObj.PatientName + "','" + patientInfoObj.Issue + "','" + patientInfoObj.ReferralDoct + "',"+patientInfoObj.EmpID+","+patientInfoObj.EmpName+",'"+patientInfoObj.PatientStatus+"')", sqlConnectionObj);
+            sqlDataAdapterObj.Fill(dataTableObj);
+            return "Doctor Information Inserted Successfully";
+        }
+        public string UpdatePatientInfo(PatientInfo patientInfoObj)
+        {
+            DataTable dataTableObj = new DataTable();
+            SqlConnection sqlConnectionObj = new SqlConnection(sqlConnectionStr);
+            SqlDataAdapter sqlDataAdapterObj = new SqlDataAdapter("Update PatientInfo set PatientName='" + patientInfoObj.PatientName + "',Issue='" + patientInfoObj.Issue + "',ReferralDoct='" + patientInfoObj.ReferralDoct + "',EmpID=" + patientInfoObj.EmpID + "',EmpName='"+patientInfoObj.EmpName+"',PatientStatus='"+patientInfoObj.PatientStatus+ "' where PatientID=" + patientInfoObj.PatientID + "", sqlConnectionObj);
+            sqlDataAdapterObj.Fill(dataTableObj);
+            return "Doctor Information Updated Successfully";
+        }
+        public DataTable EditPatientInfoByID(int PatientID)
+        {
+            DataTable dataTableObj = new DataTable();
+            SqlConnection sqlConnectionObj = new SqlConnection(sqlConnectionStr);
+            SqlDataAdapter sqlDataAdapterObj = new SqlDataAdapter(" select*from PatientInfo where PatientID=" + PatientID + "", sqlConnectionObj);
+            sqlDataAdapterObj.Fill(dataTableObj);
+            return dataTableObj;
+        }
+        public DataTable SelectPatientInfo()
+        {
+            DataTable dataTableObj = new DataTable();
+            SqlConnection sqlConnectionObj = new SqlConnection(sqlConnectionStr);
+            SqlDataAdapter sqlDataAdapterObj = new SqlDataAdapter(" select*from PatientInfo", sqlConnectionObj);
+            sqlDataAdapterObj.Fill(dataTableObj);
+            return dataTableObj;
+        }
+        public string DeletePatientInfoByID(int PatientID)
+        {
+            DataTable dataTableObj = new DataTable();
+            SqlConnection sqlConnectionObj = new SqlConnection(sqlConnectionStr);
+            SqlDataAdapter sqlDataAdapterObj = new SqlDataAdapter(" Delete from DoctorInfo where PatientID=" + PatientID + "", sqlConnectionObj);
+            sqlDataAdapterObj.Fill(dataTableObj);
+            return "Deleted Successfully";
+        }
+
+    }
+}
---
+++ HospitalManagementSystem_UI/HospitalManagementSystem_UI/Registration.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registration.aspx.cs" Inherits="HospitalManagementSystem_UI.Registration" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Hospital Management System</title>
+    <h1>Registration</h1>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="background-color:pink;">
+            <table>
+                
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="Patient ID"></asp:Label></td>
+                    <td>
+                        <asp:TextBox ID="txtPatientID" runat="server"></asp:TextBox>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td><asp:Label ID="Label2" runat="server" Text="Patient Name"></asp:Label></td>
+                    <td><asp:TextBox ID="txtPatientName" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td><asp:Label ID="Label3" runat="server" Text="Issue"></asp:Label></td>
+                    <td><asp:TextBox ID="txtIssue" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td><asp:Label ID="Label4" runat="server" Text="Referral Doctor"></asp:Label></td>
+                    <td><asp:TextBox ID="txtReferralDoctor" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td><asp:Label ID="Label5" runat="server" Text="Employee ID"></asp:Label></td>
+                    <td><asp:TextBox ID="txtEmpID" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td><asp:Label ID="Label6" runat="server" Text="Employee Name"></asp:Label></td>
+                    <td><asp:TextBox ID="txtEmpName" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td><asp:Label ID="Label7" runat="server" Text="Patient Status"></asp:Label></td>
+                    <td><asp:TextBox ID="txtPatientStatus" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="Button1" runat="server" Text="Save" OnClick="btnSave_Click" />
+                        <asp:Button ID="Button2" runat="server" Text="Edit" OnClick="btnEdit_Click"/>
+                        <asp:Button ID="Button3" runat="server" Text="Update" OnClick="btnUpdate_Click"/>
+                        <asp:Button ID="Button4" runat="server" Text="Delete" OnClick="btnDelete_Click"/>
+                        <asp:Button ID="Button5" runat="server" Text="Reset" OnClick="btnReset_Click"/>
+                    </td>
+                </tr>
+            </table>
+            <hr />
+            <asp:Label Text="" ID="lblResult" runat="server" />
+            <hr />
+            <h2>Patient Details</h2>
+            <asp:GridView ID="gvPatientInfo" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
